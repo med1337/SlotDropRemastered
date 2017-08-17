829022df8f4dbd941ef254b75561605fe4602f59
Joe da Silva <silvacore@hotmail.co.uk>
2017-08-17 19:14:12 +0100

    added a way to spawn AI (I key on keyboard)

--- Assets/Behaviours/Managers/RespawnManager.cs
+++ Assets/Behaviours/Managers/RespawnManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
 using UnityEngine;
 
 public class RespawnManager : MonoBehaviour
@@ -31,6 +32,9 @@ public class RespawnManager : MonoBehaviour
     void Update()
     {
         RespawnPlayers();
+
+        // Debug.
+        DebugSpawnAI();
     }
 
 
@@ -46,6 +50,16 @@ public class RespawnManager : MonoBehaviour
     }
 
 
+    void DebugSpawnAI()
+    {
+        if (!Input.GetKeyDown(KeyCode.I))
+            return;
+
+        USBCharacter character = CreateUSBCharacter("AIDude", Color.black);
+        character.gameObject.AddComponent<USBAI>();
+    }
+
+
     bool PlayerAwaitingRespawn(ConnectedPlayer _player)
     {
         return _player.state == PlayerState.PLAYING && _player.character == null;
@@ -54,16 +68,23 @@ public class RespawnManager : MonoBehaviour
 
     void RespawnPlayer(ConnectedPlayer _player)
     {
+        _player.character = CreateUSBCharacter("Player" + _player.id.ToString(), _player.color);
+    }
+
+
+    USBCharacter CreateUSBCharacter(string _name, Color _color)
+    {
         // Create and position the USBCharacter.
-        _player.character = Instantiate(usb_character_prefab).GetComponent<USBCharacter>();
-        _player.character.transform.position = new Vector3(0, 5, 0); // Temp.
+        USBCharacter character = Instantiate(usb_character_prefab).GetComponent<USBCharacter>();
+        character.transform.position = new Vector3(0, 5, 0); // Temp.
 
         // Configure the USBCharacter.
-        _player.character.SetColour(_player.color);
-        _player.character.name = "Player" + _player.id;
+        character.SetColour(_color);
+        character.name = _name;
 
-        //LoadoutFactory.AssignLoadout(_player.character, "Base");
-        LoadoutFactory.AssignRandomLoadout(_player.character);
+        LoadoutFactory.AssignRandomLoadout(character);
+
+        return character;
     }
 
 }

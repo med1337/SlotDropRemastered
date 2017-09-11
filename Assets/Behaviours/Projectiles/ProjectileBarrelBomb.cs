f39f983ada3818829bbd939dddbb72b8201af7d0
Jdasi <silvacore@hotmail.co.uk>
2017-09-11 16:55:24 +0100

    revised BarrelBomb ability

--- Assets/Behaviours/Projectiles/ProjectileBarrelBomb.cs
+++ Assets/Behaviours/Projectiles/ProjectileBarrelBomb.cs
@@ -5,41 +5,28 @@ using UnityEngine;
 public class ProjectileBarrelBomb : Projectile
 {
     public GameObject barrel_prefab;
+    public float dist_from_player;
+    public float y_offset;
     public float effect_radius;
     public float knockback_force;
     public float stun_duration;
-    public float start_offset_y = 5.0f;
-    public int max_bombs = 3;
     public float throw_force = 10.0f;
-
-    [Range(1000, 4000)]
-    public float min_throw_range = 2000;
-
-    [Range(1000, 4000)]
-    public float max_throw_range = 3000;
-
-    private List<Vector3> prev_drop_vectors = new List<Vector3>();
+    public float fuse_time = 3f;
+    public float barrel_torque = 5;
 
 
     void Start()
     {
-        // Hacky contingency to prevent infinite loops.
-        if (max_bombs > 5)
-            max_bombs = 5;
+        GameObject obj = Instantiate(barrel_prefab, origin + new Vector3(0, y_offset, 0), Quaternion.identity);
+        obj.transform.position += facing * dist_from_player;
+        obj.transform.rotation = Quaternion.LookRotation(-facing);
+        obj.transform.Rotate(0, 90, 0);
 
-        // Spawn bombs!
-        for (int i = 0; i < max_bombs; ++i)
-        {
-            GameObject obj = Instantiate(barrel_prefab, owner.transform.position +
-                new Vector3(0, start_offset_y, 0), Quaternion.identity);
+        Barrel bomb = obj.GetComponent<Barrel>();
 
-            Barrel bomb = obj.GetComponent<Barrel>();
-
-            ConfigureBomb(bomb);
-            GenerateDropVector(bomb);
-        }
-
-        Destroy(gameObject);
+        ConfigureBomb(bomb);
+        
+        bomb.AddForce(facing * (throw_force * 1000));
     }
 
 
@@ -49,40 +36,7 @@ public class ProjectileBarrelBomb : Projectile
         _bomb.effect_radius = this.effect_radius;
         _bomb.knockback_force = this.knockback_force;
         _bomb.stun_duration = this.stun_duration;
-    }
-
-
-    void GenerateDropVector(Barrel _bomb)
-    {
-        Vector3 drop_vector = Vector3.zero;
-
-        bool vector_valid = false;
-
-        do
-        {
-            float random_x = Random.Range(-max_throw_range, max_throw_range);
-            random_x = Mathf.Clamp(random_x, -min_throw_range, max_throw_range);
-
-            float random_z = Random.Range(-max_throw_range, max_throw_range);
-            random_z = Mathf.Clamp(random_z, -min_throw_range, max_throw_range);
-
-            drop_vector = new Vector3(random_x, throw_force * 1000, random_z);
-
-            int test_passes = 0;
-            foreach (Vector3 v in prev_drop_vectors)
-            {
-                if (Vector3.Distance(drop_vector, v) >= min_throw_range)
-                    ++test_passes;
-            }
-
-            if (test_passes >= prev_drop_vectors.Count)
-                vector_valid = true;
-
-        } while (!vector_valid);
-
-        _bomb.AddForce(drop_vector);
-
-        prev_drop_vectors.Add(drop_vector);
+        _bomb.fuse_time = this.fuse_time;
     }
 
 }
--- Assets/Behaviours/Projectiles/SpawnedObjects/Barrel.cs
+++ Assets/Behaviours/Projectiles/SpawnedObjects/Barrel.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class Barrel : MonoBehaviour
 {
+    [SerializeField] AudioSource audio_source;
+
     public GameObject shockwave_particle;
     public USBCharacter owner;
     public Rigidbody rigid_body;
 
-    public float torque_x = 15.0f;
-    public float torque_z = 12.0f;
-
     public float effect_radius;
     public float knockback_force;
     public float stun_duration;
+
+    public float fuse_time;
+    private float timer;
 
 
     public void AddForce(Vector3 force)
@@ -22,25 +24,35 @@ public class Barrel : MonoBehaviour
     }
 
 
-    void FixedUpdate()
+    void Start()
     {
-        rigid_body.AddTorque(new Vector3(torque_x, 0, torque_z));
+        audio_source.volume = AudioManager.sfx_volume;
     }
 
 
-    void OnTriggerEnter(Collider _other)
+    void Update()
     {
-        if (_other.tag == "Barrel" ||
-            _other.gameObject.layer == LayerMask.NameToLayer("Projectile") ||
-            _other.gameObject.layer == LayerMask.NameToLayer("Player") ||
+        timer += Time.deltaTime;
+
+        if (timer >= fuse_time)
+            Explode();
+    }
+
+
+    void OnCollisionEnter(Collision _other)
+    {
+        if (_other.gameObject.layer == LayerMask.NameToLayer("Floor") ||
             (owner != null && _other.transform.GetInstanceID() == owner.transform.GetInstanceID()))
         {
             return;
         }
 
-        AudioManager.PlayOneShot("explosion");
-        CameraShake.Shake(0.4f, 0.4f);
+        Explode();
+    }
 
+
+    void Explode()
+    {
         Projectile.CreateEffect(shockwave_particle, transform.position, Vector3.zero);
         RaycastHit[] elems = Physics.SphereCastAll(transform.position, effect_radius,
             Vector3.down, 0, 1 << LayerMask.NameToLayer("Player"));
@@ -54,6 +66,9 @@ public class Barrel : MonoBehaviour
 
             character.Stun(stun_duration);
         }
+
+        AudioManager.PlayOneShot("explosion");
+        CameraShake.Shake(0.4f, 0.4f);
 
         Destroy(this.gameObject);
     }

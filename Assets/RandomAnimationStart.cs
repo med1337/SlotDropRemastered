9d41b0a1106d962e21c9688c36a8b6334047ba0f
Tom Turner <tom.turner2@googlemail.com>
2017-09-11 13:48:35 +0100

    Zombie Slow Particle and spear pitt fix

---
+++ Assets/RandomAnimationStart.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomAnimationStart : MonoBehaviour
+{
+    public Animator animator;
+    public string animation_name;
+
+    void OnEnable()
+    {
+        animator.Play(animation_name, -1, Random.Range(0.0f,1.0f));
+    }
+}
